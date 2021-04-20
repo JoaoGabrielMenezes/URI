989a7f124bc76ef09bd71d4340a179aef94eacdd
Joao Gabriel <60220208+JoaoGabrielMenezes@users.noreply.github.com>
2021-04-20 01:07:28 -0300

    Update Files

--- 1004.java
+++ 1004.java
@@ -1,15 +1,31 @@
-#include <stdio.h>
+import java.io.IOException;
+import java.util.Scanner;
+
+/*
+Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. A seguir mostre a variável PROD com mensagem correspondente.   
+
+Entrada
+O arquivo de entrada contém 2 valores inteiros.
+
+Saída
+Imprima a mensagem "PROD" e a variável PROD conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade. Não esqueça de imprimir o fim de linha após o produto, caso contrário seu programa apresentará a mensagem: “Presentation Error”.
+*/
+
+public class Main {
  
-int main() {
+    public static void main(String[] args) throws IOException {
  
-    int a,b,soma;
+    int a,b,prod;
     
-    scanf("%i",&a);
-    scanf("%i",&b);
-    
-    soma = a + b;
-    
-    printf("SOMA = %i\n",soma);
+     Scanner sc = new Scanner(System.in);
+    a = sc.nextInt();
+     Scanner sc2 = new Scanner(System.in);
+    b = sc.nextInt();
  
-    return 0;
+    prod = a * b;
+    
+    System.out.printf("PROD = %d\n",prod);
+ 
+    }
+ 
 }
--- 1013.java
+++ 1013.java
@@ -1,6 +1,18 @@
 import java.io.IOException;
 import java.util.Scanner;
+/*
+Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”. Utilize a fórmula:
 
+MaiorAb=(a+b+abs(a-b))/2
+
+Obs.: a fórmula apenas calcula o maior entre os dois primeiros (a e b). Um segundo passo, portanto é necessário para chegar no resultado esperado.
+
+Entrada
+O arquivo de entrada contém três valores inteiros.
+
+Saída
+Imprima o maior dos três valores seguido por um espaço e a mensagem "eh o maior".
+*/
 public class Main {
  
     public static void main(String[] args) throws IOException {
